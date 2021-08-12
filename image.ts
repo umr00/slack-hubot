ec01bc23c8cabb0635b3e4efcc97248b6af0e1e1
Kenji Fujiwara <kenjif@tcu.ac.jp>
2021-08-12 16:21:57 +0900

    extract function to implement animate

--- image.ts
+++ image.ts
@@ -1,34 +1,44 @@
 import {App, Context} from "@slack/bolt";
-import axios, {AxiosError} from "axios";
+import axios from "axios";
+
+const googleCseId = process.env.BOLT_GOOGLE_CSE_ID;
+const googleApiKey = process.env.BOLT_GOOGLE_CSE_KEY;
 
 export function addImageHandler(app: App): void {
-  // robot.respond /(image|img)( me)? (.*)/i, (msg)
-  app.message(/^(image|img)( me)? (.*)/i, async ({context, message, say}) => {
-    let [_, cmd, me, query]: string[] = context.matches;
+  if (!googleCseId || !googleApiKey){
+    console.error("Google CSE ID or GOOGLE CSE KEY is not defined.");
+    return;
+  }
 
-    const googleCseId = process.env.BOLT_GOOGLE_CSE_ID;
-    const googleApiKey = process.env.BOLT_GOOGLE_CSE_KEY;
+  app.message(/^(image|img)( me)? (.*)/i, async({context, message, say}) => {
+    let [all, command, me, query]: string[] = context.matches;
 
-    // TODO add checking existence of CSE ID and API KEY
-
-    let url = 'https://www.googleapis.com/customsearch/v1';
-
-    try{
-      const res = await axios.get(url, {
-        params: {
-          q: query,
-          searchType: 'image',
-          safe: 'high',
-          fields: 'items(link)',
-          cx: googleCseId,
-          key: googleApiKey
-        }
-      });
-      const item = res.data.items[Math.floor(Math.random() * res.data.items.length)];
-      console.log(item.link);
-      say(item.link);
-    } catch (error){
+    let link = await getImageLink(query, false);
+    if(link){
+      say(link);
+    } else {
       say(`Oops. I had trouble searching '${query}'. Try later.`);
     }
   });
+}
+
+async function getImageLink(query: string, animated: boolean) : Promise<string | null> {
+  const url = 'https://www.googleapis.com/customsearch/v1';
+
+  try{
+    const res = await axios.get(url, {
+      params: {
+        q: query,
+        searchType: 'image',
+        safe: 'high',
+        fields: 'items(link)',
+        cx: googleCseId,
+        key: googleApiKey
+      }
+    });
+    const item = res.data.items[Math.floor(Math.random() * res.data.items.length)];
+    return item.link;
+  } catch (error){
+    return null;
+  }
 }
